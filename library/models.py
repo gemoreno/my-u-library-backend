26ac6886715aa728a9dee2b71914f4aa3c10c0f4
Gerardo Moreno <gemj98@gmail.com>
2025-07-30 13:24:22 -0600

    feat: edit Book model and add serializers for Book and CheckoutRecord

--- library/models.py
+++ library/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    year_published = models.IntegerField()
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
+    year_published = models.IntegerField(null=True, blank=True)
     genre = models.CharField(max_length=50)
     stock = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
                                 
 class CheckoutRecord(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,3 +17,4 @@ class CheckoutRecord(models.Model):
     is_returned = models.BooleanField(default=False)
     date_out = models.DateTimeField(auto_now_add=True)
     date_returned = models.DateTimeField(null=True, blank=True)
+    
