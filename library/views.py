91944641d60e3f033ff057cf97972bfefb35e04c
Gerardo Moreno <gemj98@gmail.com>
2025-07-30 13:25:27 -0600

    feat: create filtered_books API endpoint and update IRL routing to include /api/books endpoint

--- library/views.py
+++ library/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def filtered_books(request):
+    title = request.GET.get('title', '')
+    author = request.GET.get('author', '')
+    genre = request.GET.get('genre', '')
+
+    books = Book.objects.all()
+
+    if title:
+        books = books.filter(title__icontains=title)
+    if author:
+        books = books.filter(author__icontains=author)
+    if genre:
+        books = books.filter(genre__icontains=genre)
+
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
